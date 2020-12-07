5cc91ea658cd8bad165d3d3cc1b0b799bb1c548c
Rantas <robsonantas@gmail.com>
2020-12-07 11:43:41 -0300

    Buscando categorias pelo DTO

---
+++ src/main/java/com/rantas/cursospring/dto/CategoriaDto.java
@@ -0,0 +1,53 @@
+package com.rantas.cursospring.dto;
+
+import java.io.Serializable;
+
+import com.rantas.cursospring.domain.Categoria;
+
+public class CategoriaDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+
+	private Integer id;
+	private String nome;
+	
+	
+	public CategoriaDto() {
+	}
+	
+	
+
+
+	public CategoriaDto(Categoria obj) {
+		
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+	}
+
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	} 
+	
+	
+	
+	
+}
+
--- src/main/java/com/rantas/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/rantas/cursospring/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.rantas.cursospring.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rantas.cursospring.domain.Categoria;
+import com.rantas.cursospring.dto.CategoriaDto;
 import com.rantas.cursospring.services.CategoriaService;
 
 @RestController
@@ -49,6 +52,14 @@ public class CategoriaResource {
 		service.delete(id);
 	
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method= RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDto>> findAll(){
+		List<Categoria> listObj =service.findAll();
+		List<CategoriaDto> listDto = listObj.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
 	}
 	
 }

b84a450a2b2bf2278507d160ee7b495945390730
Rantas <robsonantas@gmail.com>
2020-12-13 18:48:06 -0300

    Subtotal e Total de pedido

--- src/main/java/com/rantas/cursospring/domain/Pedido.java
+++ src/main/java/com/rantas/cursospring/domain/Pedido.java
@@ -59,6 +59,14 @@ public class Pedido implements Serializable{
 			this.cliente= cliente;
 			this.enderecoDeEntrega= enderecoDeEntrega;
 		}
+		
+		public double getValorTotal() {
+			Double soma =0.0;
+			for(ItemPedido ip: itens) {
+				soma+=ip.getSubTotal();
+			}
+			return soma;
+		}
 
 		
 		

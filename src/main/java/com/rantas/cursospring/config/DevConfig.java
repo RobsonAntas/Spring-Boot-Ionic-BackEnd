9ed0a79cf5fefc22d0b094ba24876e1154c11d35
Rantas <robsonantas@gmail.com>
2020-12-26 09:47:18 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/rantas/cursospring/config/DevConfig.java
+++ src/main/java/com/rantas/cursospring/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.rantas.cursospring.services.DBservice;
+import com.rantas.cursospring.services.EmailService;
+import com.rantas.cursospring.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instatiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService(); 
 	}
 }
--- src/main/java/com/rantas/cursospring/services/DBservice.java
+++ src/main/java/com/rantas/cursospring/services/DBservice.java
@@ -115,7 +115,7 @@ public class DBservice {
 		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
 		
-		Cliente cli1 = new Cliente(null, "Maria Da Silva", "maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Robson P Antas", "robsonantas@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 	
 		Endereco e1 = new Endereco(null, "Rua Flores","300", "Apto 303", "Jardim", "38220834",cli1,cid1);
